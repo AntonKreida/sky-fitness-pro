2809ba6a052bdf1bad86dfaa1c650798b49c0f9a
denniztr <denniztr@gmail.com>
2023-11-12 18:36:58 +0300

    <HomePage> | <The main page is done, the cards need to be fixed>

---
+++ src/pages/HomePage/HomePage.styled.tsx
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+
+
+export const HomePage = styled.div`
+  width: 100vw;
+  background-color: #271a58;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 1154px;
+  padding: 30px 0 35px 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DescriptionContainer = styled.div`
+  width: 1154px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const DescriptionText = styled.div`
+  width: 830px;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+  color: #aaaaaa;
+`;
+
+export const Title = styled.h2`
+  font-size: 60px;
+  font-weight: 400;
+  line-height: 54.9px;
+  letter-spacing: -1.17px;
+  color: rgba(244, 244, 255, 1);
+  padding: 17px 0 52px 0;
+`;
+
+export const SaleStickerContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  align-self: flex-end;
+  margin-right: -50px;
+`;
+
+export const SaleStickerTitle = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.15px;
+  color: rgba(255, 128, 113, 1);
+  text-align: center;
+  position: absolute;
+  bottom: 45%;
+  left: 0;
+  right: 0;
+  transform: rotate(15.96deg);
+`;
+
+export const LogoContainer = styled.div``;
+
+export const LoginButton = styled.button`
+  display: block;
+  width: 77px;
+  height: 36px;
+  background-color: rgba(20, 13, 64, 1);
+  border: none;
+  border-radius: 46px;
+  color: #fff;
+  transition: all 0.3s;
+  &:hover {
+    background-color: rgba(45, 31, 121, 1);
+  }
+`;
+
+export const CardsContainer = styled.div`
+  /* width: 1154px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 37px; */
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+// export const CardContainer = styled.div`
+//     width: 360px;
+//     height: 480px;
+// `;
+
+// export const Card = styled.div`
+//   width: 360px;
+//   height: 480px;
+//   border-radius: 30px;
+//   background-color: rgba(255, 255, 255, 1);
+//   box-shadow: -10px 10px 15.998822212219238px 0px rgba(0, 0, 0, 0.1);
+//   box-shadow: 10px -10px 16px 0px rgba(0, 0, 0, 0.1);
+// `;
+
+export const CardImage = styled.img``;
+
+export const CardTitle = styled.h1`
+    font-size: 36px;
+    font-weight: 800;
+    line-height: 36.9px;
+    letter-spacing: -0.5px;
+`;
+
+export const Anchor = styled.button`
+  width: 147px;
+  height: 48px;
+  border: none;
+  border-radius: 46px;
+  background-color: rgba(199, 233, 87, 1);
+  color: black;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+  letter-spacing: -0.1px;
+  text-align: center;
+  margin: 34px 0 60px 0;
+  transition: all 0.3s;
+  &:hover {
+    background-color: rgba(218, 242, 137, 1);
+  }
+`;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,46 @@
 import React from 'react';
 
+import { ReactComponent as LogoWhite } from '@assets/images/logoWhite.svg';
+import { ReactComponent as SaleSticker } from '@assets/images/saleSticker.svg';
 
-export const HomePage = () => (
-  <div>HomePage</div>
-);
+import { CardsData } from './cardsData';
+import * as S from './HomePage.styled';
+
+
+export const HomePage = () => {
+  const scrollToTop = () => {
+    const header = document.getElementById('header');
+    if (header) {
+      header.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+console.log(CardsData);
+
+  return (
+    <S.HomePage>
+      <S.HeaderContainer id="header">
+        <S.LogoContainer>
+          <LogoWhite />
+        </S.LogoContainer>
+        <S.LoginButton> Войти </S.LoginButton>
+      </S.HeaderContainer>
+      <S.DescriptionContainer>
+        <S.DescriptionText>
+          <S.Paragraph>Онлайн-тренировки для занятий дома</S.Paragraph>
+          <S.Title> Начните заниматься спортом и улучшите качество жизни</S.Title>
+        </S.DescriptionText>
+        <S.SaleStickerContainer>
+          <SaleSticker />
+          <S.SaleStickerTitle>Измени своё <br /> тело за полгода</S.SaleStickerTitle>
+        </S.SaleStickerContainer>
+      </S.DescriptionContainer>
+      <S.CardsContainer>
+        { CardsData.map((card) => (
+          <S.CardImage key={ card._id } src={ card.path } />
+        )) }
+      </S.CardsContainer>
+      <S.Anchor onClick={ scrollToTop }>Наверх <span>↑</span> </S.Anchor>
+    </S.HomePage>
+  );
+};
