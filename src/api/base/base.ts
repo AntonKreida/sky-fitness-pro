fd6ad2de6bcb1af04cbf3e5c19d2fbd57cc9d2c1
denniztr <denniztr@gmail.com>
2023-11-30 21:09:08 +0300

    base.ts has changed

--- src/api/base/base.ts
+++ src/api/base/base.ts
@@ -4,11 +4,3 @@ import axios from 'axios';
 export const base = axios.create({
   baseURL: process.env.REACT_APP_DATABASE_URL
 });
-
-export const baseCourses = axios.create({
-  baseURL: `${process.env.REACT_APP_DATABASE_URL}/courses.json`
-});
-
-export const baseWorkouts = axios.create({
-  baseURL: `${process.env.REACT_APP_DATABASE_URL}/workouts.json`
-});
